7987bf3f41e676df7a06177490d267c7cdbb2a42
x02359482173 <x02359482173@tcu.gov.br>
2024-06-10 23:04:50 -0300

    feat(ws-plus): crud, handlers, ajustes

--- src/main/java/com/client/ws/plus/repository/SubscriptionTypeRepository.java
+++ src/main/java/com/client/ws/plus/repository/SubscriptionTypeRepository.java
@@ -1,7 +1,6 @@
 package com.client.ws.plus.repository;
 
 import com.client.ws.plus.model.SubscriptionType;
-import com.client.ws.plus.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType, Long> {
