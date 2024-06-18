da3f75860c68fd7211adbd645ea003a3e8767845
x02359482173 <x02359482173@tcu.gov.br>
2024-06-17 22:27:35 -0300

    feat(ws-plus): crud user, usertype, handler config, instalacao de dependencia, mapper

--- src/main/java/com/client/ws/plus/controller/UserTypeController.java
+++ src/main/java/com/client/ws/plus/controller/UserTypeController.java
@@ -3,13 +3,11 @@ package com.client.ws.plus.controller;
 import com.client.ws.plus.model.UserType;
 import com.client.ws.plus.service.UserTypeService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
