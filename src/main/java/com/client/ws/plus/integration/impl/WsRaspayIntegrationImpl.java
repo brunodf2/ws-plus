712a2873db90d10fbc91b6c2bc8d8cf594e0efb6
x02359482173 <x02359482173@tcu.gov.br>
2024-06-20 02:33:38 -0300

    feat(ws-plus): ajuste de Base64, o do tomcat depreciado, atualizado para o java.util

--- src/main/java/com/client/ws/plus/integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/plus/integration/impl/WsRaspayIntegrationImpl.java
@@ -4,13 +4,14 @@ import com.client.ws.plus.dto.wsraspay.CustomerDto;
 import com.client.ws.plus.dto.wsraspay.OrderDto;
 import com.client.ws.plus.dto.wsraspay.PaymentDto;
 import com.client.ws.plus.integration.WsRaspayIntegration;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
 
 @Component
 public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
@@ -40,13 +41,13 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
 
     @Override
     public Boolean processPayment(PaymentDto paymentDto) {
-        return null;
+        return false;
     }
 
     private static HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
         String credential = "rasmooplus:r@sm00";
-        String base64 = new String(Base64.encodeBase64(credential.getBytes(), false));
+        String base64 = Base64.getEncoder().encodeToString(credential.getBytes());
         headers.add("Authorization", "Basic " + base64);
         return headers;
     }
